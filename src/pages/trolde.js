0f6018e21bbf836ba363c082a8411530801decba
Kenneth Larsen <kla@linkfire.com>
2019-05-13 12:45:06 +0200

    lol

--- src/pages/trolde.js
+++ src/pages/trolde.js
@@ -17,7 +17,7 @@ class TroldeIndex extends React.Component {
 
           Vigtigt: Læs vores artikel om hvordan vi definerer en "trold": 
           <Link to={`/blog/Velkommen%20til%20trollefanger-projektet.`}>Hvordan definerer vi en trold?</Link>
-          <br>
+          <br/>
           Følgende liste er profiler, vi stærkt mistænker for at være oprettet gennem et botnet og/eller har som formål at skabe splid i den offentlige debat.
           <hr/>
           <ul class="trolls-list">
